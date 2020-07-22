813f5e1ed713b3a2cc7866bbc63b402fbc201f72
Randall Mendez <samendezloria@estudiantec.cr>
2020-07-21 19:06:40 -0600

    [add][web][Admin]Multiple
     * Barras de navegacion en las vistas faltantes
     * Tabla en Report y Salon View

--- Hospital/src/app/admin/pages/report/report.component.ts
+++ Hospital/src/app/admin/pages/report/report.component.ts
@@ -3,13 +3,55 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.sass']
+  styleUrls: ['./report.component.sass'],
 })
 export class ReportComponent implements OnInit {
+  // Change report type
+  showCleanlinessStatistics = false;
+  showTreatmentStatistics = false;
+  showPuntualityStatistics = false;
+  aseo: any;
+  trato: any;
+  puntualidad: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.aseo = [
+      { id: 1, nota: 2 },
+      { id: 2, nota: 1 },
+      { id: 5, nota: 4 },
+      { id: 7, nota: 3 },
+    ];
+    this.trato = [
+      { id: 1, nota: 5 },
+      { id: 2, nota: 3 },
+      { id: 5, nota: 5 },
+      { id: 7, nota: 3 },
+    ];
+    this.puntualidad = [
+      { id: 1, nota: 4 },
+      { id: 2, nota: 5 },
+      { id: 5, nota: 5 },
+      { id: 7, nota: 3 },
+    ];
   }
 
+  ngOnInit() {}
+
+  reportCleanliness() {
+    this.showCleanlinessStatistics = true;
+    this.showTreatmentStatistics = false;
+    this.showPuntualityStatistics = false;
+  }
+
+  reportTreatment() {
+    this.showCleanlinessStatistics = false;
+    this.showTreatmentStatistics = true;
+    this.showPuntualityStatistics = false;
+  }
+
+  reportPuntuality() {
+    this.showCleanlinessStatistics = false;
+    this.showTreatmentStatistics = false;
+    this.showPuntualityStatistics = true;
+  }
 }
--- Hospital/src/app/admin/pages/salon-management/salon-management.component.ts
+++ Hospital/src/app/admin/pages/salon-management/salon-management.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalonManagementComponent implements OnInit {
 
+  editSalon = false;
+  createSalon = false;
+  listSalon = true;
+  idBed = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  salonEdit(id: number) {
+    this.editSalon = true;
+    this.createSalon = false;
+    this.listSalon = false;
+    this.idBed = id;
+  }
+
+  salonCreate() {
+    this.editSalon = false;
+    this.createSalon = true;
+    this.listSalon = false;
+  }
+
+  salonList() {
+    this.editSalon = false;
+    this.createSalon = false;
+    this.listSalon = true;
   }
 
 }
