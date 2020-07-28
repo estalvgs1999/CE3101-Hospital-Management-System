2449f5f8148ff193b1be52924ea2f785bc0b366c
Olman Francisco Castro Hernández <olcastro@estudiantec.cr>
2020-07-27 22:14:13 -0600

    [add][web]Medical Records
    * Se añade a vista de historial médico

--- Hospital/src/app/app-routing.module.ts
+++ Hospital/src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ const routes: Routes = [
   {
     path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   },
-  { path: '', redirectTo: 'admin', pathMatch: 'full' },
   {
     path: 'doctor', loadChildren: () => import('./doctor/doctor.module').then(m => m.DoctorModule)
   },
   {
     path: 'patient', loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule)
-  }
+  },
+  { path: '', redirectTo: 'patient', pathMatch: 'full' }
 ];
 
 @NgModule({
--- Hospital/src/app/patient/pages/profile/profile.component.ts
+++ Hospital/src/app/patient/pages/profile/profile.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  patient = {dni: 209876, name: 'Olman', lastName: 'Castro Hernández', age: 56, sex: 'Masculino'};
+  pathology: any;
+  procedureList: any;
+
+  constructor() {
+    this.pathology = [
+      {name: 'Diabetes', medication: '3ml al día'},
+      {name: 'Diabetes', medication: '3ml al día'},
+      {name: 'Diabetes', medication: '3ml al día'},
+      {name: 'Diabetes', medication: '3ml al día'},
+      {name: 'Diabetes', medication: '3ml al día'},
+      {name: 'Diabetes', medication: '3ml al día'},
+      {name: 'Diabetes', medication: '3ml al día'}
+    ];
+    this.procedureList = [
+      {date: '23-04-2020', name: 'Cirugía', treatment: 'cjdbjhchjschdjcjdbcjhdjcjhd'},
+      {date: '23-04-2020', name: 'Cirugía', treatment: 'cjdbjhchjschdjcjdbcjhdjcjhd'},
+      {date: '23-04-2020', name: 'Cirugía', treatment: 'cjdbjhchjschdjcjdbcjhdjcjhd'},
+      {date: '23-04-2020', name: 'Cirugía', treatment: 'cjdbjhchjschdjcjdbcjhdjcjhd'},
+      {date: '23-04-2020', name: 'Cirugía', treatment: 'cjdbjhchjschdjcjdbcjhdjcjhd'},
+      {date: '23-04-2020', name: 'Cirugía', treatment: 'cjdbjhchjschdjcjdbcjhdjcjhd'}
+    ];
+  }
+
 
   ngOnInit() {
   }
