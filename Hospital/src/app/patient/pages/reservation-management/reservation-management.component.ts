60c11d5bbbbc8ab60bed2a431d3b659b57b68cae
Olman Francisco Castro Hernández <olcastro@estudiantec.cr>
2020-07-28 01:09:38 -0600

    [add][web]Reservations
    * Añadir, editar y eliminar una reservación de un paciente

--- Hospital/src/app/patient/pages/reservation-management/reservation-management.component.ts
+++ Hospital/src/app/patient/pages/reservation-management/reservation-management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Procedure } from '../../interfaces/procedure';
 
 @Component({
   selector: 'app-reservation-management',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationManagementComponent implements OnInit {
 
-  constructor() { }
+  procedureList: any;
+  edit = false;
+  add = false;
+  patient = {dni: 209876, name: 'Olman', lastName: 'Castro Hernández', age: 56, sex: 'Masculino'};
+  proceduresNameList: any;
+  procedureNameSelectList: any[] = [];
+
+  constructor() {
+    this.procedureList = [
+      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+    ];
+    this.proceduresNameList = [
+      {name: 'jndjcd'},
+      {name: 'jbgbgbgb'},
+      {name: 'jnd456jcd'},
+      {name: 'jndjcnmmmmmd'},
+      {name: 'jndjccccccd'},
+    ];
+  }
 
   ngOnInit() {
+  }
+
+  editProcedure() {
+    this.edit = true;
+  }
+
+  back() {
+    this.edit = false;
+    this.add = false;
+  }
+
+  // Add a procedure in the list for send to Data Base
+  getProcedureValue(value: Procedure[]): void {
+    const i = this.procedureNameSelectList.indexOf(value);
+
+    if (i === -1) {
+      this.procedureNameSelectList.push(value);
+    }
+
+  }
+
+  // Delete a procedure for the list
+  deleteProcedure(value: Procedure): void {
+    const i = this.procedureNameSelectList.indexOf(value);
+
+    if (i !== -1) {
+      this.procedureNameSelectList.splice(i, 1);
+    }
   }
 
 }
