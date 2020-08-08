0c4c874f5c73b53dfa120a87a55697cb84bc8b60
Olman Francisco Castro Hernández <olcastro@estudiantec.cr>
2020-08-07 18:44:32 -0600

    [add][Doctor-view]Add patient
    *Correción al añadir un paciente

--- Hospital/src/app/app.module.ts
+++ Hospital/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +18,9 @@ import { PatientModule } from './patient/patient.module';
     AppRoutingModule,
     AdminModule,
     DoctorModule,
-    PatientModule
+    PatientModule,
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Hospital/src/app/doctor/pages/patient-register/patient-register.component.ts
+++ Hospital/src/app/doctor/pages/patient-register/patient-register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pathology } from '../../interfaces/Pathology';
+import { LocationService } from '../../services/location.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-patient-register',
@@ -10,10 +12,18 @@ export class PatientRegisterComponent implements OnInit {
 
   pathology: Pathology;
   pathologyList: any[] = [];
+  provinces: any;
+  provincesKey: string;
+  cantons: any;
+  distrits: any;
 
-  constructor() { }
+  // tslint:disable-next-line: variable-name
+  constructor(private _http: LocationService) { }
 
   ngOnInit() {
+    this._http.getProvince().subscribe(data => {
+      this.provinces = data;
+    });
   }
 
   // Add a pathology to the list
@@ -36,5 +46,38 @@ export class PatientRegisterComponent implements OnInit {
       this.pathologyList.splice(i, 1);
     }
   }
+
+send(name: string, lastName: string, dni: number, password: string,
+     date: string, phone: string, province: string, canton: string, distrit: string, address: string) {
+
+ console.log(name, lastName, dni, password, date, phone, province, canton, distrit, address);
+}
+
+getCanton(cantonId: string) {
+ // tslint:disable-next-line: forin
+ for (const key in this.provinces) {
+   const value = this.provinces[key];
+   if (value === cantonId) {
+     this.provincesKey = key;
+     this._http.getCanton(key).subscribe(data => {
+       this.cantons = data;
+     });
+   }
+
+}
+}
+
+getDistrit(distritId: string) {
+
+ // tslint:disable-next-line: forin
+ for (const key in this.cantons) {
+   const value = this.cantons[key];
+   if (value === distritId) {
+     this._http.getDistrit(this.provincesKey, key).subscribe(data => {
+       this.distrits = data;
+     });
+   }
+ }
+}
 
 }
