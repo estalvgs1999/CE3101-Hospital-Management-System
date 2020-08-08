5fe18d3320752845c67464b7037b8edb315b8bb2
BertaBrenes <bertacaro1996@gmail.com>
2020-08-07 22:46:54 -0600

    [add][hospital]view update

--- Hospital/src/app/core/services/reservation.service.ts
+++ Hospital/src/app/core/services/reservation.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
+    getAllReservation() {
+      return this.http.get(`${environment.API.HOSPITAL}/reservation`, {observe: 'response'});
+    }
+    getReservationByPatient(dni) {
+      return this.http.get(`${environment.API.HOSPITAL}/reservation/${dni}`, {observe: 'response'});
+    }
+    getProcedureByReservation(dni) {
+      return this.http.get(`${environment.API.HOSPITAL}/reservation/procedures/${dni}`, {observe: 'response'});
+    }
+    createReservation(data: object) {
+      return this.http.post(`${environment.API.HOSPITAL}/reservation`, data, {observe: 'response'});
+    }
+    deleteReservation(id: string) {
+      return this.http.delete(`${environment.API.HOSPITAL}/reservation/${id}`, {observe: 'response'});
+    }
+    updateReservation(id:string, data: object){
+      return this.http.patch(`${environment.API.HOSPITAL}/reservation/${id}`, data, {observe: 'response'});
+    }
 }
--- Hospital/src/app/patient/pages/reservation-management/reservation-management.component.ts
+++ Hospital/src/app/patient/pages/reservation-management/reservation-management.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Procedure } from '../../interfaces/procedure';
+import { ReservationService } from 'src/app/core/services/reservation.service';
+import { BedService } from 'src/app/core/services/bed.service';
+import { ProcedureService } from 'src/app/core/services/procedure.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-management',
@@ -8,35 +12,65 @@ import { Procedure } from '../../interfaces/procedure';
 })
 export class ReservationManagementComponent implements OnInit {
 
-  procedureList: any;
+  ReservationList: any;
   edit = false;
   add = false;
-  patient = {dni: 209876, name: 'Olman', lastName: 'Castro Hernández', age: 56, sex: 'Masculino'};
+  patient = {dni: '1-7745-8956', name: 'Olman', lastName: 'Castro Hernández', age: 56, sex: 'Masculino'};
   proceduresNameList: any;
+  procedureAddList: any = [];
   procedureNameSelectList: any[] = [];
   patientInformation = true;
+  beds: any = [];
+  editReservation: any;
 
-  constructor() {
-    this.procedureList = [
-      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
-      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
-      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
-      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
-      {arrivalDate: '23-04-2020', departureDate: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+  constructor(
+    private reservationService: ReservationService,
+    private bedService: BedService,
+    private procedureService: ProcedureService,
+    private router: Router) {
+    this.ReservationList = [
+      {arrival_date: '23-04-2020', departure_date: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+      {arrival_date: '23-04-2020', departure_date: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+      {arrival_date: '23-04-2020', departure_date: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+      {arrival_date: '23-04-2020', departure_date: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
+      {arrival_date: '23-04-2020', departure_date: '05-05-2020', procedures: ['uno', 'dos', 'tres', 'cuatro']},
     ];
     this.proceduresNameList = [
       {name: 'jndjcd'},
       {name: 'jbgbgbgb'},
       {name: 'jnd456jcd'},
       {name: 'jndjcnmmmmmd'},
-      {name: 'jndjccccccd'},
+      {name: 'jndjccccccd'}
     ];
   }
 
   ngOnInit() {
+    this.bedService.getAllBeds().subscribe( bedResponse => {
+      console.log('beds', bedResponse);
+      this.beds = bedResponse.body;
+    });
+    this.procedureService.getAllProcedure().subscribe( procedureResponse => {
+      this.proceduresNameList = procedureResponse.body;
+      this.procedureAddList =  procedureResponse.body;
+      console.log('procedure', this.proceduresNameList);
+    });
+    this.reservationService.getReservationByPatient(this.patient.dni).subscribe( res => {
+      console.log('res', res);
+      this.ReservationList = res.body;
+      // tslint:disable-next-line: forin
+      for (const key in res.body) {
+        console.log(res.body[key]['id']);
+        this.reservationService.getProcedureByReservation(res.body[key]['id']).subscribe( resP => {
+          console.log('procedure res', resP);
+          this.ReservationList[key]['procedures'] = resP.body;
+        });
+      }
+    });
   }
 
-  editProcedure() {
+  editProcedure(reservation: object, procedureList: object[]) {
+    this.editReservation = reservation;
+    this.procedureNameSelectList = procedureList;
     this.edit = true;
     this.patientInformation = false;
   }
@@ -74,5 +108,34 @@ export class ReservationManagementComponent implements OnInit {
       this.procedureNameSelectList.splice(i, 1);
     }
   }
-
+  getProcedureByResId(id: string) {
+    this.reservationService.getProcedureByReservation(id).subscribe(res => {
+      console.log('res procedure', res);
+      return res.body;
+    });
+  }
+  deleteReservation(id: string) {
+    this.reservationService.deleteReservation(id).subscribe( delRes => {
+      console.log('delete Response', delRes);
+      window.location.reload();
+    });
+  }
+  createReservation(date: string, bedNumber: string) {
+    const data = {
+      PatientDni: this.patient.dni,
+      BedID: bedNumber,
+      ArrivalDate: date,
+      Procedures: this.procedureNameSelectList
+    };
+    console.log('data create', data);
+    this.reservationService.createReservation(data).subscribe(addRes => {
+      console.log('create Res', addRes);
+      window.location.reload();
+    });
+  }
+  updateReservation(data: object, id: string) {
+    this.reservationService.updateReservation(id, data).subscribe( upRess => {
+      console.log('update res', upRess);
+    });
+  }
 }
